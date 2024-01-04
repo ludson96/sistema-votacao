d61fa3cf31e4626d90d7db32ec8f34ea5d5fb67f
Ludson Pereira <ludson_ps27@hotmail.com>
2024-01-04 19:39:41 -0300

    feat: create class Gerenciamento votação all methods

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -4,15 +4,9 @@ import java.util.ArrayList;
 
 public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
-  private ArrayList<PessoaCandidata> pessoasCandidatas;
-  private ArrayList<PessoaEleitora> pessoasEleitoras;
-  private ArrayList<String> cpfsComputados;
-
-  public GerenciamentoVotacao() {
-    this.pessoasCandidatas = new ArrayList<>();
-    this.pessoasEleitoras = new ArrayList<>();
-    this.cpfsComputados = new ArrayList<>();
-  }
+  private ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<>();
+  private ArrayList<PessoaEleitora> pessoasEleitoras = new ArrayList<>();
+  private ArrayList<String> cpfsComputados = new ArrayList<>();
 
   @Override
   public void cadastrarPessoaCandidata(String nome, int numero) {
@@ -23,28 +17,51 @@ public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
       }
     }
 
-    PessoaCandidata newCandidata = new PessoaCandidata(nome, numero);
-    this.pessoasCandidatas.add(newCandidata);
+    this.pessoasCandidatas.add(new PessoaCandidata(nome, numero));
   }
 
   @Override
   public void cadastrarPessoaEleitora(String nome, String cpf) {
-    for (PessoaEleitora cpfPessoa : pessoasEleitoras) {
+    for (PessoaEleitora cpfPessoa : this.pessoasEleitoras) {
       if (cpfPessoa.getCpf().equals(cpf)) {
         System.out.println("Pessoa eleitora já cadastrada!");
         return;
       }
     }
 
-    PessoaEleitora newEleitora = new PessoaEleitora(nome, cpf);
-    pessoasEleitoras.add(newEleitora);
+    this.pessoasEleitoras.add(new PessoaEleitora(nome, cpf));
   }
 
   @Override
   public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+    if (this.cpfsComputados.contains(cpfPessoaEleitora)) {
+      System.out.println("Pessoa eleitora já votou!");
+      return;
+    }
+
+    for (PessoaCandidata candidata : pessoasCandidatas) {
+      if (candidata.getNumero() == numeroPessoaCandidata) {
+        candidata.receberVoto();
+      }
+    }
+
+    this.cpfsComputados.add(cpfPessoaEleitora);
   }
 
   @Override
   public void mostrarResultado() {
+    if (cpfsComputados.isEmpty()) {
+      System.out.println("É preciso ter pelo menos um voto para mostrar o resultado.");
+      return;
+    }
+
+    for (PessoaCandidata candidata : pessoasCandidatas) {
+      int percent = (candidata.getVotos() / cpfsComputados.size()) * 100;
+
+      System.out.printf("Nome: %s - %d votos ( %d%n", candidata.getNome(), candidata.getVotos(),
+          percent);
+    }
+
+    System.out.printf("Total de votos: %d%n", this.cpfsComputados.size());
   }
 }
--- src/main/java/com/betrybe/sistemadevotacao/Principal.java
+++ src/main/java/com/betrybe/sistemadevotacao/Principal.java
@@ -3,8 +3,14 @@ package com.betrybe.sistemadevotacao;
 public class Principal {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    GerenciamentoVotacao teste1 = new GerenciamentoVotacao();
 
+    teste1.cadastrarPessoaCandidata("Maria", 12345);
+    teste1.cadastrarPessoaCandidata("José", 54321);
+
+    teste1.cadastrarPessoaEleitora("João", "111.222.333.444-55");
+
+    teste1.votar("111.222.333.444-55", 12345);
+    teste1.mostrarResultado();
   }
-
 }
